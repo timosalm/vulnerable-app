b6f98d9e5d04cb2fd486f5ebae8c738087d6114d
Timo Salm <tsalm@pivotal.io>
2021-10-26 11:04:57 +0200

    first commit

---
+++ src/test/java/com/example/vulnerableapp/VulnerableAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vulnerableapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulnerableAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
